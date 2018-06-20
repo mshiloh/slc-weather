a4713dfee47a07b7bbff09ed1103d92f7623beb0
mshiloh <mshiloh.code@gmail.com>
2018-06-20 13:55:56 -0600

    debugging host issue

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/ping", function (req, res) {
 //     res.sendFile(path.join(__dirname, "build", "index.html"));
 // });
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
